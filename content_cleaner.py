42cb1f60226b22c41d9f0ce720368a2d90743627
Sylvain Chambon <sylvain.chambon@openwide.fr>
2015-12-08 10:21:35 +0100

    make double sure not to delete double linebreaks

--- content_cleaner.py
+++ content_cleaner.py
@@ -1,14 +1,24 @@
 import re, warnings
 
+DEBUG = False
+
 def clean(content):
     content = despan(content)
+    if DEBUG: print("\n1. -> %s " % content)
     content = re.sub(r'<p[^>]*>', r'', content)
+    if DEBUG: print("\n2. -> %s " % content)
     content = re.sub(r'</p>', r'\n\n', content)
+    if DEBUG: print("\n3. -> %s " % content)
     content = re.sub(r'  +', r' ', content) # remove double spaces
+    if DEBUG: print("\n4. -> %s " % content)
     content = re.sub(r'\t', '', content) # remove tabs
+    if DEBUG: print("\n5. -> %s " % content)
     content = re.sub(r'<blockquote>', r'> ', content)
+    if DEBUG: print("\n6. -> %s " % content)
     content = re.sub(r'<[^>]+>', r'', content)
-    content = re.sub(r'\s*\n', r'\n', content)
+    if DEBUG: print("\n7. -> %s " % content)
+    content = re.sub(r'[ \t]*\n', r'\n', content)
+    if DEBUG: print("\n8. -> %s " % content)
     return content
 
 def despan(text):
@@ -46,7 +56,9 @@ def despan(text):
     return re.sub(r'<span class="rg_texte-modificateur"[^/]*</span>', r'', result)
 
 if __name__ == "__main__":
-    text = '''L'AMF, consultée par écrit préalablement à la réalisation d'une opération et sur une question relative à l'interprétation <span class="rg_texte-modificateur">(Arrêté du 15 avril 2005)</span> « du présent règlement (ci-après désigné « le RG ») », rend un avis sous forme de rescrit. Cet avis précise si, au regard des éléments communiqués par l'intéressé, l'opération n'est pas contraire au présent règlement. L'AMF, consultée par écrit préalablement à la réalisation d'une opération et sur une question relative à l'interprétation <span class="rg_texte-modificateur">(Arrêté du 15 avril 2005)</span> « du <span class="rg_texte-modificateur">(pouet)</span> « présent » règlement », rend un avis sous forme de rescrit. Cet avis précise si, au regard des éléments communiqués par l'intéressé, l'opération n'est pas contraire au présent règlement.'''
+    DEBUG = True
+
+    text = '''<p class="rg_modif-partie"></p><p class="rg_modif-partie">(modifié par arrêté du 20 août 2010, Journal officiel du 28 août 2010)</p><p class="rg_modif-partie">(modifié par arrêté du 2 avril 2009, Journal officiel du 5 avril 2009)</p><p class="rg_modif-partie">(modifié par arrêté du 27 décembre 2007, Journal officiel du 30 décembre 2007)</p><p class="rg_modif-partie">(modifié par arrêté du 11 septembre 2007, Journal officiel du 27 septembre 2007)</p><p class="rg_modif-partie">(modifié par arrêté du 1er septembre 2005, Journal officiel du 8 septembre 2005)</p><p class="rg_modif-partie">(modifié par arrêté du 15 avril 2005, Journal officiel du 22 avril 2005)</p><p class="rg_modif-partie">(modifié par arrêté du 12 novembre 2004, Journal officiel du 24 novembre 2004)</p><p class="rg_modif-partie">(homologué par arrêté du 12 octobre 2004, Journal officiel du 29 octobre 2004)'''
 
     print("Initial text:\n{}".format(text))
     print("\nFinal text:\n{}".format(clean(text)))
